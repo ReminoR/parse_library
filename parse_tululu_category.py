66f0f3d9b4aa54957205bae1a58e589727396315
Victor Shamakov <sva1.0@mail.ru>
2022-09-01 20:11:03 +0700

    The program separated on small functions. Added reconnection counter and reconnection. Added progress bar. Changed descriptions and exceptions. Changed variable names

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -8,105 +8,107 @@ import time
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from pathvalidate import sanitize_filename, sanitize_filepath
+from tqdm import tqdm
 from tululu import download_txt, parse_book_page, download_image, check_for_redirect
 
 
 DOMAIN = "https://tululu.org/"
 VERSION = "1.0"
+reconnection_counter = 0
 
 
-def download_category(url_category, start_page, end_page, dest_folder="./", skip_txt=False, skip_imgs=False, json_path="./"):
-    """Функция для скачивания категорий книг.
+def get_number_pages(url):
+    """Функция получает количество страниц в категории или автора
     Args:
-        category_number (int): id категории, которую собираетесь скачать
-        start_page (int): начиная с какой страницы скачивать книги
-        end_page (int): заканчивая какой страницей скачивать книги 
-        books_folder (str): Папка, куда сохранять книги
-        skip_txt (bool): не скачивать книги
-        images_folder (str): Папка, куда сохранять обложки книг
-        skip_imgs (bool): не скачивать картинки
-        json_path (str): указать путь для json файла
+        url (int): url категории
     Returns:
-        str: ссылки на скаченные книги
+        int: количество страниц в категории
     """
+    global reconnection_counter
+    success_connection = False
 
-    collection = []
-    os.makedirs(json_path, exist_ok=True)
-
-    for page in range(start_page, end_page):
+    while not success_connection:
         try:
-            response = requests.get(urljoin(url_category, str(page)))
+            response = requests.get(url)
+            response.raise_for_status()
+            success_connection = True
+            reconnection_counter == 0
+
+            soup = BeautifulSoup(response.text, "lxml")
+            pages = soup.select(".npage")
+            if pages:
+                npage = int(soup.select(".npage")[-1].text)
+            else:
+                npage = 1
+
+            return npage
+        except requests.exceptions.HTTPError:
+            print(f'HTTPError. The page is not exists', file=sys.stderr)
+            quit()
+
+        except requests.exceptions.ConnectionError:
+            if reconnection_counter == 0:
+                print('Get number page is failed. ConnetionError. Reconnection attempt', file=sys.stderr)
+                reconnection_counter = reconnection_counter + 1
+                get_number_pages(url)  
+            else:
+                print('Get number page is failed. ConnetionError. Reconnection attempt (3 sec.)', file=sys.stderr)
+                time.sleep(3)
+                reconnection_counter = reconnection_counter + 1
+                get_number_pages(url)
+
+
+def get_links(url, start_page, end_page):
+    """Функция возвращает ссылки на книги определенной категории или автора
+    Args:
+        url (int): url категории
+    Returns:
+        int: количество страниц в категории
+    """
+    global reconnection_counter
+    book_links = []
+
+    for page in tqdm(range(start_page, end_page + 1)):
+        try:
+            response = requests.get(urljoin(url, str(page)))
             response.raise_for_status()
             check_for_redirect(response, DOMAIN)
 
             soup = BeautifulSoup(response.text, "lxml")
-            books_links = soup.select(".d_book .bookimage a")
-
-
-            for book_link in books_links:
-                book_href = book_link["href"]
-                book_id = re.search("\d+", book_href)[0]
-
-                try:
-                    book_url = urljoin(DOMAIN, book_href)
-                    response = requests.get(book_url)
-                    book = parse_book_page(response.text)
-                    path_to_books = sanitize_filepath(os.path.join(dest_folder, "books", book["title"]))
-                    book["book_path"] = f'{path_to_books}.txt'
-                    params = {"id": book_id}
-                    book_url_txt = f'{DOMAIN}txt.php'
-
-                    if not skip_txt:
-                        download_txt(book_url_txt, params, book['title'], os.path.join(dest_folder, 'books'))
-
-                    if not skip_imgs:
-                        download_image(urljoin(DOMAIN, book['img_src']), book['img_name'], os.path.join(dest_folder, 'img'))
-                    
-                    collection.append(book)
-                    
-
-                except requests.exceptions.HTTPError:
-                    print(f'HTTPError. The book id {book_id} is not exists', file=sys.stderr)
-
-                except requests.exceptions.ConnectionError:
-                    print('ConnetionError', file=sys.stderr)
-                    time.sleep(1)
-
-                collection_json = json.dumps(collection, ensure_ascii=False, indent=4)
-
-                with open(os.path.join(json_path, "books.json"), "w", encoding='utf8') as books:
-                    books.write(collection_json)
+            links = soup.select(".d_book .bookimage a")
+            book_links.append([link['href'] for link in links])
 
         except requests.exceptions.HTTPError:
-            print(f'HTTPError. The page {page} is not exists', file=sys.stderr)
+            print(f'HTTPError. The page number {page} is not exists')
 
         except requests.exceptions.ConnectionError:
-            print(f'ConnetionError', file=sys.stderr)
-            time.sleep(1)
+            if reconnection_counter == 0:
+                print('Get links are failed. ConnetionError. Reconnection attempt', file=sys.stderr)
+                reconnection_counter = reconnection_counter + 1
+                get_links(url, page, end_page) 
+            else:
+                print('Get links are failed. ConnetionError. Reconnection attempt (3 sec.)', file=sys.stderr)
+                time.sleep(3)
+                reconnection_counter = reconnection_counter + 1
+                get_links(url, page, end_page)
 
+    book_links = sum(book_links, [])
 
-def get_number_pages(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    check_for_redirect(response, DOMAIN)
+    return book_links
 
-    soup = BeautifulSoup(response.text, "lxml")
-    npage = int(soup.select(".npage")[-1].text)
-
-    return npage
 
 def create_parser():
     parser = argparse.ArgumentParser(
-            prog = 'Parser of books category from library https://tululu.org/',
+            prog = 'Parser of books from library https://tululu.org/ by category/author',
             description = '''Parser for category of books and downloading''',
             epilog = '''(c) SVA 2022 GNU licensed'''
     )
-    parser.add_argument ('--id', nargs='?', type=int, default=55, help="Id of books category")
-    parser.add_argument ('-s', '--start_page', nargs='?', type=int, default=1, help="Page number for start of downloading from category")
-    parser.add_argument ('-e', '--end_page', nargs='?', type=int, default=10, help="Page number for finish of downloading from category")
+    parser.add_argument ('-p', '--path', required=True, type=str, help="Path to page of books category. For example: 'l55/', 'fantastic/', 'a8146/'")
+    parser.add_argument ('-s', '--start_page', nargs='?', type=int, default=1, help="Page number for start of downloading from category. Default: 1")
+    parser.add_argument ('-e', '--end_page', nargs='?', type=int, help="Page number for finish of downloading from category. Default is the last page of category. Default: start_page + 1")
     parser.add_argument ('-d', '--dest_folder', nargs='?', type=str, default="./", help="Destination folder for books and images")
-    parser.add_argument ('-t', '--skip_txt', action="store_true", default=False, help="Skip donwloaind of books")
-    parser.add_argument ('-i', '--skip_imgs', action="store_true", default=False, help="Skip donwloaind of images")
+    parser.add_argument ('-t', '--skip_txt', action="store_true", default=False, help="Skip donwloaind of books. Default: False")
+    parser.add_argument ('-i', '--skip_imgs', action="store_true", default=False, help="Skip donwloaind of images. Default: False")
     parser.add_argument ('-j', '--json_path', nargs='?', type=str, default="./", help="Destination for json file")
 
 
@@ -119,16 +121,67 @@ def create_parser():
 
 
 def main():
+    global reconnection_counter
     parser = create_parser()
     args = parser.parse_args()
-    url = f"https://tululu.org/l{args.id}/"
-
+    url = urljoin(DOMAIN, args.path)
     npage = get_number_pages(url)
+    collection = []
 
-    if args.end_page > npage:
+    if args.end_page == None:
+        args.end_page = args.start_page + 1
+
+    if args.end_page > npage or args.start_page > npage:
+        args.start_page = npage
         args.end_page = npage
 
-    download_category(url, args.start_page, args.end_page, dest_folder = args.dest_folder, skip_txt=args.skip_txt, skip_imgs=args.skip_imgs, json_path=args.json_path)
+    if args.end_page < args.start_page or args.start_page <= 0 or args.end_page <= 0:
+        print("Error. Указан некорректный диапазон страниц", file=sys.stderr)
+        quit()
+
+    book_links = get_links(url, args.start_page, args.end_page)
+
+    for book_link in tqdm(book_links):
+        success_connection = False
+        while not success_connection:
+            try:
+                book_url = urljoin(DOMAIN, book_link)
+                response = requests.get(book_url)
+                reconnection_counter = 0
+                success_connection = True
+                book_description = parse_book_page(response.text)
+                path_to_books = sanitize_filepath(os.path.join(args.dest_folder, "books", book_description["title"]))
+                book_description["book_path"] = f'{path_to_books}.txt'
+
+                book_id = re.search("\d+", book_link)[0]
+                params = {"id": book_id}
+                book_txt_url = f'{DOMAIN}txt.php'
+
+                if not args.skip_txt:
+                    download_txt(book_txt_url, params, book_description['title'], os.path.join(args.dest_folder, 'books'))
+
+                if not args.skip_imgs:
+                    download_image(urljoin(DOMAIN, book_description['img_src']), book_description['img_name'], os.path.join(args.dest_folder, 'img'))
+                
+                collection.append(book_description)
+
+            except requests.exceptions.HTTPError:
+                print(f'HTTPError. The book id {book_id} is not exists', file=sys.stderr)
+                reconnection_counter = 0
+
+            except requests.exceptions.ConnectionError:
+                if reconnection_counter == 0:
+                    print('Downloading of book is failed. ConnetionError. Reconnection attempt', file=sys.stderr)
+                    reconnection_counter = reconnection_counter + 1
+                else:
+                    print('Downloading of book is failed. ConnetionError. Reconnection attempt (3 sec.)', file=sys.stderr)
+                    time.sleep(3)
+                    reconnection_counter = reconnection_counter + 1
+
+    collection_json = json.dumps(collection, ensure_ascii=False, indent=4)
+
+    with open(os.path.join(args.json_path, "books.json"), "w", encoding='utf8') as books:
+        books.write(collection_json)
 
 
 if __name__ == "__main__":
